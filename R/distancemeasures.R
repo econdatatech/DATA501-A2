2793d0ccc0da1d72763939c3f172a442c7e7450b
Corvin Idler <corvin.idler@gmail.com>
2024-08-14 10:35:07 +1100

    fixed Hadi

--- R/distancemeasures.R
+++ R/distancemeasures.R
@@ -1,5 +1,5 @@
 
-distances <- function(data, model, measure='cooks'){
+distances <- function(data, model, plots=TRUE){
   #input validation
   if (!is.data.frame(data)) {
     stop("The input data must be of type data.frame.")
@@ -8,6 +8,26 @@ distances <- function(data, model, measure='cooks'){
   if (class(model) != "lm") {
     stop("The input model must be an object of class lm")
   }
+
+  if (class(plots) != "logical") {
+    stop("The input parameter plots must be a boolean (object of class logical) so FALSE or TRUE")
+  }
+
+  cooks<-cooks_distance_lm(model)
+  dffits<-dffits_lm(model)
+  hadi<-hadi_lm(model)
+
+  if(plots){
+
+  # divide frame in 2X2 grid
+  par( mfrow= c(2,2) )
+
+  # draw 4 plots
+  plot(cooks)
+  plot(dffits)
+  plot(hadi)
+  }
+  return(list(cooks=cooks,dffits=dffits,hadi=hadi))
 
   ##do more input data validations...
   ## do we need consistency checks between input data and what is in the model?
@@ -41,7 +61,7 @@ cooks_distance_lm <- function(model){
 
 
 #based on https://avys.omu.edu.tr/storage/app/public/rezzanu/141865/[David_A._Belsley,_Edwin_Kuh,_Roy_E._Welsch]_Regre(BookFi.org).pdf
-#based on https://github.com/SurajGupta/r-source/blob/master/src/library/stats/R/lm.influence.R
+#and https://github.com/SurajGupta/r-source/blob/master/src/library/stats/R/lm.influence.R
 dffits_lm <- function(model){
 
   hat = lm.influence(model, do.coef=FALSE)$hat
@@ -54,15 +74,13 @@ dffits_lm <- function(model){
   return(dffits)
 }
 
-#Still need to dynamically estimate p= number of predictor vars (it's not always 1!!)
-#summary(model)$df[1] - 1 is number of betas without intercept
-#or length(coef(fit))-1
-
 ##based on equation 3.7 in Hadi 1992 and 4.9.3 in Regression-Analysis-by-Example
 hadi_lm <- function(model) {
   h <- hatvalues(model)
   #based on sentence under equation 3.7
   di <- residuals(model)/sqrt(sum(residuals(model)^2))
-  h/(1-h) + 2/(1-h) * di^2/(1-di^2)
+  p<-length(coef(model))-1
+  return(h/(1-h) + (p+1)/(1-h) * di^2/(1-di^2))
+
 }
 
