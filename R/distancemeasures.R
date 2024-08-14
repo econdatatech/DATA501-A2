f11e79914fdb1f3e68dba31b999928577f48fabf
Corvin Idler <corvin.idler@gmail.com>
2024-08-14 13:15:01 +1100

    bugfixes

--- R/distancemeasures.R
+++ R/distancemeasures.R
@@ -1,4 +1,3 @@
-
 #' distances
 #'
 #' @param data A Dataframe
@@ -8,10 +7,10 @@
 #' @return a list with names entries (cooks,dffits,hadi)
 #' @export
 #'
-#' @examples distances(mtcars,model)
-distances <- function(data, model, plots=TRUE){
+#' @examples x1 <- rnorm(100, 50, 9); y1 <-5*x1+1; randomdata <- data.frame(y1, x1); model <- lm(y1 ~ x1, data = randomdata); distances <- function(data, model, plots = TRUE)
 
-  #input validation
+distances <- function(data, model, plots=TRUE){
+# input validation
   if (!is.data.frame(data)) {
     stop("The input data must be of type data.frame.")
   }
@@ -24,73 +23,69 @@ distances <- function(data, model, plots=TRUE){
     stop("The input parameter plots must be a boolean")
   }
 
-  cooks<-cooks_distance_lm(model)
-  dffits<-dffits_lm(model)
-  hadi<-hadi_lm(model)
+  cooks <- cooks_distance_lm(model)
+  dffits <- dffits_lm(model)
+  hadi <- hadi_lm(model)
 
-  if(plots){
-
-  # divide frame in 2X2 grid
-  par( mfrow= c(2,2) )
-  # draw 4 plots
-  plot(cooks,layout = NULL)
-  plot(dffits,layout = NULL)
-  plot(hadi,layout = NULL)
+  if (plots) {
+    # divide frame in 2X2 grid
+    par(mfrow = c(2, 2))
+    # draw 4 plots
+    plot(cooks, layout = NULL)
+    plot(dffits, layout = NULL)
+    plot(hadi, layout = NULL)
   }
-  return(list(cooks=cooks,dffits=dffits,hadi=hadi))
+  return(list(cooks = cooks, dffits = dffits, hadi = hadi))
 
-  ##do more input data validations...
+  ## do more input data validations...
   ## do we need consistency checks between input data and what is in the model?
   ## check limitations of each method... e.g. https://www.statology.org/dffits-in-r/
-  #e.g. A size-adjusted cutoff recommended by Belsley, Kuh, and Welsch is 2*sqrt(p/n)
-  #not sure why the data also had to be provided to the function.
-  #are we not able to get everything we need from the model itself?
-  #Hadi measure also has a cut off value@!
+  # e.g. A size-adjusted cutoff recommended by Belsley, Kuh, and Welsch is 2*sqrt(p/n)
+  # not sure why the data also had to be provided to the function.
+  # are we not able to get everything we need from the model itself?
+  # Hadi measure also has a cut off value@!
 }
 
-#based on https://doi.org/10.2307/1268249
-#https://doi.org/10.1080/00401706.1977.10489493
+# based on https://doi.org/10.2307/1268249
+# https://doi.org/10.1080/00401706.1977.10489493
 
-#From: https://github.com/SurajGupta/r-source/blob/master/src/library/stats/R/lm.influence.R
-cooks_distance_lm <- function(model){
-          #as per page 15 of Cook 1977 (above equation 1)
-          resid = weighted.residuals(model) #to allow for weighted regression
-          #residual degrees of freedom (number of observ.
-          #minus number of regression coefficients)
-          df= df.residual(model)
-          sd = sqrt(deviance(model)/df)
-          #diagonals of the ‘hat’ matrix.
-          hat = lm.influence(model, do.coef=FALSE)$hat
+# From: https://github.com/SurajGupta/r-source/blob/master/src/library/stats/R/lm.influence.R
+cooks_distance_lm <- function(model) {
+  # as per page 15 of Cook 1977 (above equation 1)
+  resid <- weighted.residuals(model) # to allow for weighted regression
+  # residual degrees of freedom (number of observ.
+  # minus number of regression coefficients)
+  df <- df.residual(model)
+  sd <- sqrt(deviance(model) / df)
+  # diagonals of the ‘hat’ matrix.
+  hat <- lm.influence(model, do.coef = FALSE)$hat
 
-          p <- model$rank
-          #equation 7 in Cook 1977
-          D <- ((resid/(sd * sqrt((1 - hat)))^2 * hat)/(p*(1 - hat)))
-          D[is.infinite(D)] <- NaN
-          return(D)
-  }
+  p <- model$rank
+  # equation 7 in Cook 1977
+  D <- ((resid / (sd * sqrt((1 - hat)))^2 * hat) / (p * (1 - hat)))
+  D[is.infinite(D)] <- NaN
+  return(D)
+}
 
 
-#based on https://avys.omu.edu.tr/storage/app/public/rezzanu/141865/[David_A._Belsley,_Edwin_Kuh,_Roy_E._Welsch]_Regre(BookFi.org).pdf
-#and https://github.com/SurajGupta/r-source/blob/master/src/library/stats/R/lm.influence.R
-dffits_lm <- function(model){
-
-  hat = lm.influence(model, do.coef=FALSE)$hat
-  sigma = lm.influence(model, do.coef=FALSE)$sigma
-  res = weighted.residuals(model)
-  #based on equation 2.11 on page 15 of Belsley 1980
-  #re-written to avoid one sqrt(1-hat)
-  dffits <- res * sqrt(hat)/(sigma*(1-hat))
+# based on https://avys.omu.edu.tr/storage/app/public/rezzanu/141865/[David_A._Belsley,_Edwin_Kuh,_Roy_E._Welsch]_Regre(BookFi.org).pdf
+# and https://github.com/SurajGupta/r-source/blob/master/src/library/stats/R/lm.influence.R
+dffits_lm <- function(model) {
+  hat <- lm.influence(model, do.coef = FALSE)$hat
+  sigma <- lm.influence(model, do.coef = FALSE)$sigma
+  res <- weighted.residuals(model)
+  # based on equation 2.11 on page 15 of Belsley 1980
+  # re-written to avoid one sqrt(1-hat)
+  dffits <- res * sqrt(hat) / (sigma * (1 - hat))
   dffits[is.infinite(dffits)] <- NaN
   return(dffits)
 }
 
-##based on equation 3.7 in Hadi 1992 and 4.9.3 in Regression-Analysis-by-Example
+## based on equation 3.7 in Hadi 1992 and 4.9.3 in Regression-Analysis-by-Example
 hadi_lm <- function(model) {
   h <- hatvalues(model)
-  #based on sentence under equation 3.7
-  di <- residuals(model)/sqrt(sum(residuals(model)^2))
-  p<-length(coef(model))-1
-  return(h/(1-h) + (p+1)/(1-h) * di^2/(1-di^2))
-
+  # based on sentence under equation 3.7
+  di <- residuals(model) / sqrt(sum(residuals(model)^2))
+  p <- length(coef(model)) - 1
+  return(h / (1 - h) + (p + 1) / (1 - h) * di^2 / (1 - di^2))
 }
-
--- tests/testthat/test-distances.R
+++ tests/testthat/test-distances.R
@@ -1,30 +1,35 @@
-x1=rnorm(100,50,9)
-x2=rnorm(100,200,64)
-error=rnorm(100,0,16)
-#Generate the dependent variable (b0=150, b1=-4, b2=2.5)
-y1=150-(4*x1)+(2.5*x2)+error
+x1 <- rnorm(100, 50, 9)
+x2 <- rnorm(100, 200, 64)
+error <- rnorm(100, 0, 16)
+# Generate the dependent variable (b0=150, b1=-4, b2=2.5)
+y1 <- 150 - (4 * x1) + (2.5 * x2) + error
 
-randomdata<- data.frame(y1, x1, x2)
+randomdata <- data.frame(y1, x1, x2)
 # Fit a linear regression model
 # Predicting y1 based on x1 and x2
 model <- lm(y1 ~ x1 + x2, data = randomdata)
 
 test_that("we test wrong data type of the first parameter of distances()", {
-  expect_error(distances(data=c(2, 3, NA, 2, 4, 4),model=model),
-
-                 'The input data must be of type data.frame.')
+  expect_error(
+    distances(data = c(2, 3, NA, 2, 4, 4), model = model),
+    "The input data must be of type data.frame."
+  )
 })
 
 test_that("we test wrong data type of the second parameter of distances()", {
-  expect_error(distances(data=randomdata,model=TRUE),
-               'The input model must be an object of class lm')
+  expect_error(
+    distances(data = randomdata, model = TRUE),
+    "The input model must be an object of class lm"
+  )
 })
 
 test_that("we test wrong data type of the third parameter of distances()", {
-  expect_error(distances(data=randomdata,model=model,plots='VERYTRUE'),
-               'The input parameter plots must be a boolean')
+  expect_error(
+    distances(data = randomdata, model = model, plots = "VERYTRUE"),
+    "The input parameter plots must be a boolean"
+  )
 })
 
 test_that("We test distances() with correct input", {
-  expect_no_error(distances(data=randomdata,model=model,plots=TRUE))
+  expect_no_error(distances(data = randomdata, model = model, plots = TRUE))
 })
